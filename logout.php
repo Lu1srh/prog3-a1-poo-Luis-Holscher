684d77836bb22214fba682c678c8b2d697933172
Luis H <129464443+Lu1srh@users.noreply.github.com>
2025-04-15 20:16:28 -0300

    ajuste logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'classes/Sessao.php';
-
+Sessao::iniciar();
 Sessao::destruir();
-header("Location: login.php");
+header('Location: login.php?sucesso=Logout realizado com sucesso!');
 exit;
 ?>
